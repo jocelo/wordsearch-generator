a07ab5eabb18788cce45947223739ebfeb74b2a5
Alfredo Alonso <jocelo@Alfredos-MBP.fios-router.home>
2019-03-31 02:00:02 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule
